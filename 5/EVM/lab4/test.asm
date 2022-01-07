a711188397478378ef2025ac39b6d4393ef8ab81
Andreev Dmitry <g.andreevm.d@gmail.com>
2022-01-07 11:28:04 +0300

    Доработаны лабораторные по Архитектуре ЭВМ

--- 5/EVM/lab4/test.asm
+++ 5/EVM/lab4/test.asm
@@ -1,4 +1,4 @@
-Data SEGMENT
+Data SEGMENT		; Сегмент данных
 
 ; Вариант 3
 ; Решить уравнение вида:
@@ -11,41 +11,40 @@ Data SEGMENT
 	X DW ?
 Data ENDS
 
-Ourstack SEGMENT Stack
-	DB 100h DUP (?)
+Ourstack SEGMENT Stack	; Сегмент стека
+		; Выделили 100h (256) байт под стек
+	DB 100h DUP (?)	
 Ourstack ENDS
 
+		; Привязываем имена к регистрам
 ASSUME CS:Code, DS:Data, SS:Ourstack 
 
-Code SEGMENT
-
+Code SEGMENT		; Сегмент кода
 Start:
 
-	mov AX, Data
+	mov AX, Data	; Загрузка сегмента данных
 	mov DS, AX
-	xor AX, AX	; Подготовили сегмент данных
+	xor AX, AX	; Очистка регистра AX
 			
 	mov AL, A	; Делим A на 5 используя регистры AL и BL
-	mov BL, 5	; с сохранение ответа в AL
-	div BL	
+	mov BL, 5	; с сохранением ответа в AH:AL
+	div BL		; по условию используем только частное (AL)
 			
-	mov BL, AL	; Сохраняем полученный ответ в BL
+	mov BL, AL	; Сохраняем полученный ответ (частное) в BL
 			
-	add BL, 1	; Прибавляем к X 1
-	mov AL, B	; Сохраняем В == 111b в AL 
+	add BL, 1	; Прибавляем к полученному ответу 1
+	mov AL, B	; Сохраняем (В == 111b) в AL 
 	mul BL		; и умножаем на полученный при вычислениях выше результат в AL
 	mov X, AX	; Сохраняем результат в X
 	
-	mov AL, C	; Вычисляем (C * D) с использованием регистров AL и BL
-	mov BL, D
-	mul BL
+	mov AL, C	
+	mov BL, D	; Вычисляем (C * D) с использованием регистров AL и BL
+	mul BL		; результат умножения сохраняется в AX
 	
 	sub X, AX	; Полученное произведение складываем с результатом вычеслений в X
 			; Получили ответ, завершаем программу.
 	mov AL, 0
 	mov AH, 4Ch
-	int 21h
-	
+	int 21h	
 Code ENDS
-
 END Start
