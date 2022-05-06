5ba6ea1f33a3596a27c504bbaf802f5a24ca249b
Andreev Dmitriy <g.andreevm.d@gmail.com>
2022-05-06 22:21:37 +0300

    [DataBase] template for lab6

--- 6/DataBase/lab_6/procedures.sql
+++ 6/DataBase/lab_6/procedures.sql
@@ -1,89 +1,42 @@
-# table of params
+DROP FUNCTION insert_vertex(character varying,integer,real,real,integer,integer);
+CREATE OR REPLACE FUNCTION insert_vertex(
+	vname character varying,
+	vgraph int,
+	vtlx real,
+	vtly real,
+	vsize int,
+	vlink int
+)
+	RETURNS boolean
+	LANGUAGE 'plpgsql'
+AS $$	
+	DECLARE
+		new_vert_id integer;
+		
+	BEGIN
+		
+		if not exists (SELECT graph_id FROM graphs WHERE graph_id = vgraph) of vsize <= 0 then
+			RETURN false;
+		end if;
+		
+		new_vert_id := (
+			SELECT max(vr.vertex_id) + 1
+			FROM vertexes as vr
+		);
+		
+		INSERT INTO vertexes (vertex_name, graph_id, top_left_x, top_left_y, vert_size)
+		VALUES (vname, vgraph, vtlx, vtly, vsize);
+		
+		if vlink IS NOT NULL then
+			INSERT INTO vertex_identity (start_vertex, end_vertex)
+			VALUES (new_vert_id, vlink);
+		end if;
+		return true;
+		
+	END
+$$
 
-DROP TABLE IF EXISTS vertex_params;
-CREATE TABLE IF NOT EXISTS vertex_params (
-	params_id 				serial NOT NULL,				# unique id	
-	top_left_x				real NOT NULL,					# x of top left corner
-	top_left_y				real NOT NULL,					# y of top left corner
-	vert_size				integer NOT NULL,				# size of vertex
-	
-	PRIMARY KEY (params_id)	
-);
-
-
-# table of authors
-
-DROP TABLE IF EXISTS authors;
-CREATE TABLE IF NOT EXISTS authors (
-	author_id				serial NOT NULL, 				# unique id
-	author_name				varchar(50) DEFAULT NULL,			# author name
-	author_surname				varchar(50) DEFAULT NULL,			# author surname
-	
-	PRIMARY KEY (author_id)			
-);
-
-DROP TABLE IF EXISTS graphs;
-CREATE TABLE IF NOT EXISTS graphs (
-	graph_name				varchar(50) NOT NULL UNIQUE,
-	author_id				integer DEFAULT NULL,
-	
-	PRIMARY KEY (graph_name),
-	
-	FOREIGN KEY (author_id)
-		REFERENCES
-		authors(author_id) ON DELETE CASCADE
-);
-
-
-# table of vertexes
-
-DROP TABLE IF EXISTS vertexes;
-CREATE TABLE IF NOT EXISTS vertexes (
-	vertex_id 				serial NOT NULL,				# unique id
-	vertex_name				varchar(50) NOT NULL,				# vertex name
-	vertex_params_id			integer NOT NULL,				# vertex parameters table id
-	graph_id				integer DEFAULT NULL,				# graphs table id
-	
-	PRIMARY KEY (vertex_id),
-	
-	FOREIGN KEY (vertex_params_id) 
-		REFERENCES 
-		vertex_params(params_id)  ON DELETE RESTRICT,
-	FOREIGN KEY (graph_id) 								
-		REFERENCES
-		graphs(graph_id)  ON DELETE CASCADE
-);
-
-
-# table of links
-
-DROP TABLE IF EXISTS links;
-CREATE TABLE IF NOT EXISTS links (
-	link_id				serial NOT NULL,				# unique id
-	start_vertex				integer NOT NULL,				# vertex where link starts
-	end_vertex				integer DEFAULT NULL,				# vertex where link ends
-	
-	PRIMARY KEY (link_id),
-	
-	FOREIGN KEY (start_vertex)
-		REFERENCES
-		vertexes(vertex_id),
-	FOREIGN KEY (end_vertex)
-		REFERENCES
-		vertexes(vertex_id)
-);
-
-
-# function to fill vertexes table
-
-DROP FUNCTION IF EXISTS fill_schema(character varying, 
-									character varying, 
-									real, 
-									real, 
-									integer, 
-									character varying, 
-									character varying);
-CREATE OR REPLACE FUNCTION public.fill_schema(
+CREATE OR REPLACE FUNCTION fill(
 	gn character varying,
 	vn character varying,
 	tlx real,
@@ -217,117 +170,3 @@ AS $$
 	RETURN QUERY SELECT * FROM vertexes;
 END 
 $$;
-
-
-
-# fill vertexes table
-SELECT * FROM fill_schema('graph of ivan', 'first vert', 0.1, 1.32, 10, 'Ivan', 'Ivanov');
-SELECT * FROM fill_schema('graph of ivan', 'отказ',	5.1, 6.6, 11, 'Ivan', 'Ivanov');
-SELECT * FROM fill_schema('graph of ivan', 'содержит отказ', 9.2, 15.15, 43, 'Ivan', 'Ivanov');
-
-SELECT * FROM fill_schema('second graph of ivan', 'содержит отказ или нет', 9.2, 15.15, 43, 'Ivan', 'Ivanov');
-
-SELECT * FROM fill_schema('graph of andrew', 'sec vert', 1.31, 2.61, 1, 'Andrew', 'Andreev');
-SELECT * FROM fill_schema('graph of andrew', 'third vert',	6.12, 11.1, 15, 'Andrew', 'Andreev');
-SELECT * FROM fill_schema('graph of andrew', 'эта ошибка в середине', 3.14, 14.1, 11, 'Andrew', 'Andreev');
-
-SELECT * FROM fill_schema('graph of artem', 'ошибка', 14.5, 13.3, 5, 'Artem', 'Bazhenov');
-SELECT * FROM fill_schema('graph of artem', 'этот отказ в середине', 12.5, 1.3, 4, 'Artem', 'Bazhenov');
-SELECT * FROM fill_schema('graph of artem', 'отказ в начале',	1.5, 5.1, 16, 'Artem', 'Bazhenov');
-
-
-# truncate cascade 
-TRUNCATE vertexes CASCADE;
-TRUNCATE authors CASCADE;
-TRUNCATE graphs CASCADE;
-TRUNCATE vertex_params CASCADE;
-TRUNCATE links CASCADE
-
-# drop tables
-DROP TABLE IF EXISTS vertexes CASCADE;
-DROP TABLE IF EXISTS graphs CASCADE;
-DROP TABLE IF EXISTS authors CASCADE;
-DROP TABLE IF EXISTS vertex_params CASCADE;
-DROP TABLE IF EXISTS links CASCADE;
-
-# add and remove coulumn
-ALTER TABLE authors ADD age integer
-ALTER TABLE authors DROP COLUMN age
-
-# increese column values
-UPDATE vertex_params SET vert_size=vert_size+1
-
-# delete row
-DELETE FROM authors WHERE author_name='Ivan'
-
-# merge (not supported by psql)
-MERGE INTO authors as au USING vertexes as v
-on au.author_id=v.author_id
-WHEN MATCHED
-THEN UPDATE SET name='Ivan', surname='Ivanov'
-WHEN NOT MATCHED and v.name='ошибка'
-THEN INSERT VALUES ('Roman', 'Romanov')
-
-# а. Вершины, название/текст которых содержит слово «отказ», но не зканчивается им
-SELECT * FROM vertexes WHERE vertex_name~*'отказ +'
-
-
-# б. Вершины, у которых нет исходящих ребер
-SELECT v.*
-FROM vertexes AS v, links AS l
-WHERE v.vertex_id = l.start_vertex AND l.end_vertex IS NULL
-
-
-# в. Графы, в которых есть пара вершин, связанных ребрами в обе стороны
-
-
-
-# г. Ширина графа в пикселях (от максимальной сумма координаты по горизонтали с шириной отнять минимальную левую координату)
-SELECT max_cor.max_x + max_cor.vert_size - min_cor.min_x as graph_size
-FROM (
-	SELECT top_left_x as max_x, vert_size, params_id
-	FROM vertex_params
-	WHERE top_left_x = (
-		SELECT DISTINCT max(top_left_x)
-		FROM vertex_params
-	)
-) as max_cor, (
-	SELECT DISTINCT min(top_left_x) as min_x
-	FROM vertex_params
-) as min_cor
-
-
-# д. пользователи-авторы графов с максимальных количеством вершин
-SELECT gr.graph_name, au.author_name, au.author_surname, counts.vert_count
-FROM (
-	SELECT count(*) AS vert_count, v.graph_id as graph_id
-	FROM vertexes as v
-	GROUP BY graph_id
-) as counts,
-graphs as gr
-RIGHT JOIN authors as au ON gr.author_id = au.author_id
-WHERE counts.vert_count = (
-	SELECT max(max_counts.vert_count)
-	FROM (
-		SELECT count(*) AS vert_count, v.graph_id as graph_id
-		FROM vertexes as v
-		GROUP BY graph_id
-	) AS max_counts
-) AND gr.graph_name = counts.graph_id
-
-
-# е. Вершины, для которых есть исходящие ребра, ведущие ко всем остальным вершинам еѐ графа
-
-
-
-# ж. Вершина, у которой нет входящих ребер от вершины о словом «ошибка»
-SELECT * FROM vertexes
-WHERE vertex_id != ALL(
-	SELECT l.end_vertex 
-	FROM links as l, vertexes as v
-	WHERE 
-		l.end_vertex IS NOT NULL AND 
-		l.start_vertex = v.vertex_id AND 
-		v.vertex_name~*'ошибка'
-)
-
