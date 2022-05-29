f3c8d9894d9ac7c9e6bd29ef73bc6cead76b74d0
MrAnderson <g.andreevm.d@gmail.com>
2022-05-29 19:40:42 +0300

    [PE] add lab 2 files

--- 6/PE/2/cgi_script/main.cpp
+++ 6/PE/2/cgi_script/main.cpp
@@ -11,31 +11,22 @@
 //pqxx
 #include <pqxx/pqxx>
 
-using json_map = std::map<std::string, std::string>;
-using key_val = std::pair<std::string, std::string>;
+struct Train_info;
 
-std::string make_json(json_map & map);
-json_map load_trains();
+using train_info_vec = std::vector<Train_info>;
+
+std::string make_json(train_info_vec &);
+train_info_vec load_trains(int);
+int parceQuery(cgicc::Cgicc &);
 
 int main(int argc, char ** argv) 
 {
-#ifdef DEBUG
-    // json_map test_data;
-    // test_data.insert(std::pair<std::string, std::string>("some_train_1", "some_train_time_1"));
-    // test_data.insert(std::pair<std::string, std::string>("some_train_2", "some_train_time_2"));
-    // test_data.insert(std::pair<std::string, std::string>("some_train_3", "some_train_time_3"));
-    // test_data.insert(std::pair<std::string, std::string>("some_train_4", "some_train_time_4"));
-    // test_data.insert(std::pair<std::string, std::string>("some_train_5", "some_train_time_5"));
-    // test_data.insert(std::pair<std::string, std::string>("some_train_6", "some_train_time_6"));
-
-    // make_json(test_data);
-#endif
-
     try
     {
         cgicc::Cgicc cgi;
         std::cout << cgicc::HTTPContentHeader("application/json") << std::endl;
-        auto trains = load_trains();
+        auto selection = parceQuery(cgi);
+        auto trains = load_trains(selection);
         auto result = make_json(trains);
         std::cout << result;
     }
@@ -46,22 +37,53 @@ int main(int argc, char ** argv)
     }
 }
 
-json_map load_trains()
+struct Train_info
+{
+    int id;
+    std::string name;
+    std::string time;
+};
+
+int parceQuery(cgicc::Cgicc & cgi)
+{
+    int selection = -1;
+    cgicc::form_iterator id = cgi.getElement("select");
+    if (id != cgi.getElements().end())
+    {
+        selection = id->getIntegerValue(0);
+    }
+
+    return selection;
+}
+
+train_info_vec load_trains(int selection)
 {
     try 
     {
         const std::string conn_info = "postgresql://train_user:user_pass@127.0.0.1/railroad?connect_timeout=10";
 
-        json_map trains_info;
+        train_info_vec trains_info;
         pqxx::connection conn(conn_info);
         pqxx::work job(conn);
 
         auto res = job.exec("SELECT * FROM trains");
         job.commit();
 
+        std::cerr << "filtered id:" << selection;
+
         for (auto const & row : res)
         {
-            trains_info.insert(key_val(row[1].as<std::string>(), row[2].as<std::string>()));
+            int id = row[0].as<int>();
+            if (selection >= 0 && id != selection) {
+                continue;
+            }
+            
+            Train_info ti = {
+                id,
+                row[1].as<std::string>(),
+                row[2].as<std::string>()
+            };
+            trains_info.push_back(ti);
         }
 
         return trains_info;
@@ -73,45 +95,31 @@ json_map load_trains()
     }
 }
 
-// cmake_minimum_required(VERSION 3.7)
-// project(CGI_TEST)
-
-// find_package(cgicc)
-// find_package(pqxx)
-
-// add_executable(CGI_TEST main.cpp)
-
-std::string make_json(json_map & map)
+std::string make_json(train_info_vec & v)
 {
     const std::string begin_array_token = "[\n";
     const std::string end_array_token = "]\n";
-    const std::string begin_block_token = "{\n";
-    const std::string end_block_token = "},\n";
-    const std::string end_token = ",\n";
-    const std::string tab_token = "\t";
-    const std::string endl_token = "\n";
     
     std::ostringstream json_stream;
 
     json_stream << begin_array_token;
-
-    for (auto it = map.begin(); it != --map.end(); ++it)
+    for (auto it = v.begin(); it != v.end(); ++it)
     {
-        json_stream << tab_token << begin_block_token;
-        json_stream << tab_token << tab_token << it->first << ": " << it->second << endl_token;
-        json_stream << tab_token << end_block_token;
+        json_stream << "\t{\n";
+        json_stream << "\t\t\"id\":\"" << it->id << "\",\n";
+        json_stream << "\t\t\"name\":\"" << it->name << "\",\n";
+        json_stream << "\t\t\"time\":\"" << it->time << "\"\n";
+
+        if (it == --v.end())
+        {
+            json_stream << "\t}\n";
+        }
+        else
+        {
+            json_stream << "\t},\n";
+        }
     }
-
-    auto pair = *(--map.end());
-    json_stream << tab_token << begin_block_token;
-    json_stream << tab_token << tab_token << pair.first << ": " << pair.second << endl_token;
-    json_stream << tab_token << "}\n";
-
     json_stream << end_array_token;
-
-#ifdef DEBUG
-    // std::cout << json_stream.str();
-#endif
 
     return json_stream.str();
 }
